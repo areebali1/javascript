9715547172cf19c3142093a1500447b9efbeb0e1
areebali1 <areeb6239@gmail.com>
2024-07-05 12:38:14 +0500

    Add files via upload

---
+++ New folder/script.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+
+// string 
+// number 
+// Boolean
+// undefined
+// null
+// Symbol
+// bigint
+// Object
+
+
+// let num1 = 68
+// let num2 = 5
+// let ans = num1 % num2
+// console.log(ans);
+
+// post increment 
+// pre increment 
+
+
+
+// post decrement 
+// pre decrement 
+
+let num1 = "20"
+let num2 = 20
+let ans = num1 !== num2
+console.log(ans);
